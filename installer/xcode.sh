247ccd391d2bd66c666d2e177334e9ef22b7b928
Marcel Hoppe <5444908+hoppsen@users.noreply.github.com>
2020-11-24 11:13:03 +0100

    Changed Xcode script since you would need to manually install the Command Line Tools for git anyway

--- installer/xcode.sh
+++ installer/xcode.sh
@@ -4,23 +4,10 @@
 # Command Line Tools for Xcode
 #
 
-if xcode-select --print-path &> /dev/null; then
-    printf "\e[93mCommand Line Tools already installed!\e[m\n"
+if defaults read com.apple.dt.Xcode &> /dev/null; then
+    printf "\e[93mCommand Line Tools license already accepted!\e[m\n"
     exit -1
 fi
 
-# Prompt user to install the Command Line Tools for Xcode
-xcode-select --install &> /dev/null
-
-# Wait until the Command Line Tools for Xcode are installed
-until xcode-select --print-path &> /dev/null; do
-sleep 5
-done
-
-print_result $? 'Install Command Line Tools'
-
-# Point the `xcode-select` developer directory to the appropriate directory from within `Xcode.app`
-sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-print_result $? 'Make "xcode-select" developer directory point to Xcode'
-
+# Accept Command Line Tools license
 sudo xcodebuild -license accept
